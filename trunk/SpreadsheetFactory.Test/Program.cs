d33d9f35cd412e0279cbd34c9d0174a9a112eb18
italoneypessoa@gmail.com <italoneypessoa@gmail.com@d338f9bc-1bf0-d329-d169-b257fd53d8a8>
2014-06-11 18:34:38 +0000

    biblioteca para gerenciar criacao de planilhas customizadas

---
+++ trunk/SpreadsheetFactory.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetFactory.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Header header = new Header();
+            header.AddFilter("nome", "italo");
+            header.AddFilter("idade", 1);
+            header.AddFilter("nome", "jose");
+        }
+    }
+}
