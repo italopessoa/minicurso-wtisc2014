12fa29fcdaf04313f520e83ea52679af2ea77525
italoneypessoa <italoneypessoa@gmail.com>
2014-06-11 18:34:38 +0000

    biblioteca para gerenciar criacao de planilhas customizadas

---
+++ SpreadsheetFactory/Header.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpreadsheetFactory
+{
+    public class Header
+    {
+        private IDictionary<string, object> _filters;
+        private string _title;
+
+        public IDictionary<string,object> Filters
+        {
+            get { return _filters; }
+            //set { _filters = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public bool AddFilter(string key, object value)
+        {
+            if (ValidadeHeaderValues(key, value))
+            {
+                if (_filters == null)
+                {
+                    _filters = new Dictionary<string, object>();
+                }
+
+                if (!_filters.Keys.Contains(key))
+                {
+                    _filters.Add(key, value);
+                }
+                
+                return true;
+            }
+            return false;
+        }
+
+        private bool ValidadeHeaderValues(string key, object value)
+        {
+            return true;
+        }
+
+    }
+}
