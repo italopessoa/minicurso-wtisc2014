94806a880ee8562c3a1b3a9073861d34d5a58451
italoneypessoa@gmail.com <italoneypessoa@gmail.com@d338f9bc-1bf0-d329-d169-b257fd53d8a8>
2014-06-16 20:52:02 +0000

    metodo para aplicar formatacao criado titulo e conteud criados configurar formatacao condicional (nativa ou implementada)

--- trunk/SpreadsheetFactory.Test/Program.cs
+++ trunk/SpreadsheetFactory.Test/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.HSSF.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,7 +27,6 @@ namespace SpreadsheetFactory.Test
             spf.TableHeaders = new List<TableHeader>();
 
             #region table header
-
 
             TableHeader ta = new TableHeader();
             ta.Text = "A";
@@ -58,9 +59,7 @@ namespace SpreadsheetFactory.Test
 
             #endregion table header
 
-
             #region teste final
-
 
             //TableHeader ta = new TableHeader();
             //ta.Text = "A";
@@ -117,6 +116,168 @@ namespace SpreadsheetFactory.Test
 
             #endregion
 
+            #region teste brutal
+
+            //TableHeader ta = new TableHeader();
+            //ta.Text = "A";
+
+            //TableHeader tb = new TableHeader();
+            //tb.Text = "B";
+            //tb.AddSpanCell("1");
+            //tb.AddSpanCell("2");
+
+            //TableHeader tc = new TableHeader();
+            //tc.Text = "C";
+
+            //TableHeader td = new TableHeader();
+            //td.Text = "D";
+
+            //TableHeader te = new TableHeader();
+            //te.Text = "E";
+
+            //TableHeader tf = new TableHeader();
+            //tf.Text = "F";
+
+            //TableHeader tg = new TableHeader();
+            //tg.Text = "G";
+
+            //TableHeader th = new TableHeader();
+            //th.Text = "H";
+
+            //TableHeader ti = new TableHeader();
+            //ti.Text = "I";
+
+            //TableHeader tj = new TableHeader();
+            //tj.Text = "J";
+            //tj.AddSpanCell("3");
+            //tj.AddSpanCell("4");
+
+            //TableHeader tk = new TableHeader();
+            //tk.Text = "K";
+            //tk.AddSpanCell("5");
+            //tk.AddSpanCell("6");
+
+            //TableHeader tl = new TableHeader();
+            //tl.Text = "L";
+            //tl.AddSpanCell("7");
+            //tl.AddSpanCell("8");
+
+            //TableHeader tm = new TableHeader();
+            //tm.Text = "M";
+            //tm.AddSpanCell("9");
+            //tm.AddSpanCell("10");
+
+            //TableHeader tn = new TableHeader();
+            //tn.Text = "N";
+            //tn.AddSpanCell("11");
+            //tn.AddSpanCell("12");
+
+            //TableHeader to = new TableHeader();
+            //to.Text = "O";
+            //to.AddSpanCell("13");
+            //to.AddSpanCell("14");
+
+            //TableHeader tp = new TableHeader();
+            //tp.Text = "P";
+            //tp.AddSpanCell("15");
+            //tp.AddSpanCell("16");
+
+            //TableHeader tq = new TableHeader();
+            //tq.Text = "Q";
+            //tq.AddSpanCell("17");
+            //tq.AddSpanCell("18");
+
+
+            //#region tc
+
+            //tc.Cells = new List<TableHeader>();
+            //td.Cells = new List<TableHeader>();
+            //te.Cells = new List<TableHeader>();
+            //tf.Cells = new List<TableHeader>();
+            //tg.Cells = new List<TableHeader>();
+            //th.Cells = new List<TableHeader>();
+            //ti.Cells = new List<TableHeader>();
+
+            //#region td
+
+            //tf.Cells.Add(tj);
+            //tf.Cells.Add(tk);
+
+            //tg.Cells.Add(tl);
+            //tg.Cells.Add(tm);
+
+            //td.Cells.Add(tf);
+            //td.Cells.Add(tg);
+
+            //#endregion td
+
+            //#region te
+
+            //th.Cells.Add(tn);
+            //th.Cells.Add(to);
+
+            //ti.Cells.Add(tp);
+            //ti.Cells.Add(tq);
+
+            //te.Cells.Add(th);
+            //te.Cells.Add(ti);
+
+            //#endregion te
+
+
+            //tc.Cells.Add(td);
+            //tc.Cells.Add(te);
+            //#endregion tc
+
+            //spf.TableHeaders.Add(ta);
+            //spf.TableHeaders.Add(tb);
+            //spf.TableHeaders.Add(tc);
+
+            #endregion
+
+            List<object> list = new List<object>();
+            list.Add(new Pessoa() { Idade = 12, Nascimento = DateTime.Now, Nome = "Italo", Salario = 2 });
+            list.Add(new Pessoa() { Idade = 12, Nascimento = DateTime.Now, Nome = "Italo", Salario = 10.3 });
+            list.Add(new Pessoa() { Idade = 12, Nascimento = DateTime.Now, Nome = "Italo", Salario = 10.3 });
+            list.Add(new Pessoa() { Idade = 12, Nascimento = DateTime.Now, Nome = "Italo", Salario = 10.3 });
+
+            string[] properties = new string[4];
+            properties[0] = "Idade";
+            properties[1] = "Nascimento";
+            properties[2] = "Nome";
+            properties[3] = "Salario";
+
+            spf.Datasource = list;
+            spf.Properties = properties;
+
+
+            HSSFCellStyle headerStyle = WorkbookManager.GetNewHSSFCellStyle();
+            headerStyle.BorderTop = 1;
+            headerStyle.BorderRight = 1;
+            headerStyle.BorderRight = 1;
+            headerStyle.BorderBottom = 1;
+
+            headerStyle.Alignment = HSSFCellStyle.ALIGN_CENTER;
+            headerStyle.VerticalAlignment = HSSFCellStyle.VERTICAL_CENTER;
+            headerStyle.FillPattern = HSSFCellStyle.SOLID_FOREGROUND;
+            headerStyle.FillForegroundColor = HSSFColor.BLUE.index;
+
+            HSSFFont font = WorkbookManager.GetNewHSSFCellFont();
+            font.Color = HSSFColor.WHITE.index;
+            font.Boldweight = HSSFFont.BOLDWEIGHT_BOLD;
+            headerStyle.SetFont(font);
+
+            WorkbookManager.SetHeaderCellStyle(headerStyle);
+
+            HSSFCellStyle contentStyle = WorkbookManager.GetNewHSSFCellStyle();
+            contentStyle.BorderTop = 1;
+            contentStyle.BorderRight = 1;
+            contentStyle.BorderRight = 1;
+            contentStyle.BorderBottom = 1;
+            contentStyle.FillPattern = HSSFCellStyle.SOLID_FOREGROUND;
+            contentStyle.FillForegroundColor = HSSFColor.YELLOW.index;
+
+            WorkbookManager.SetDefaultContentCellStyle(contentStyle);
 
             WorkbookManager.CreateSpreadsheet(spf);
             //Console.WriteLine("FIM");
@@ -127,3 +288,4 @@ namespace SpreadsheetFactory.Test
 
    
 }
+
